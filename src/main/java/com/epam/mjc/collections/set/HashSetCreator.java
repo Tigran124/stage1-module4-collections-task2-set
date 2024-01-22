b0ec3e05e58e1824582a19ae3e5f56ab069bd5aa
Tigran124 <tigran.h.988@gmail.com>
2024-01-22 20:13:45 +0400

    changes completed

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,18 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> set = new HashSet<>();
+        for (Integer x: sourceList){
+            set.add(x);
+            if (x % 2 == 0){
+                do {
+                    x /= 2;
+                    set.add(x);
+                }while (x % 2 == 0);
+            }else {
+                set.add(2 * x);
+            }
+        }
+        return set;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,8 +2,17 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        Set<Integer> set = new TreeSet<>();
+        for (Integer x: sourceList){
+            x *= x;
+            if (x >= lowerBound && x<= upperBound){
+                set.add(x);
+            }
+        }
+        return set;
     }
 }
